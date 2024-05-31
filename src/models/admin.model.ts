28f53ddc464ba80045dac762b3e97beab398e844
Rugved-Technify <rugvedr39@gmail.com>
2024-05-31 12:45:19 +0530

    lasst time
    lasst time

--- src/models/admin.model.ts
+++ src/models/admin.model.ts
@@ -1,23 +1,7 @@
 import { DataTypes, Model, Optional } from 'sequelize';
 import { sequelize } from '../config/database';
 
-interface AdminAttributes {
-  id: number;
-  username: string;
-  password: string;
-  createdAt?: Date;
-  updatedAt?: Date;
-}
-
-interface AdminCreationAttributes extends Optional<AdminAttributes, 'id' | 'createdAt' | 'updatedAt'> {}
-
-class Admin extends Model<AdminAttributes, AdminCreationAttributes> implements AdminAttributes {
-  public id!: number;
-  public username!: string;
-  public password!: string;
-  public readonly createdAt!: Date;
-  public readonly updatedAt!: Date;
-}
+class Admin extends Model {}
 
 Admin.init(
   {
