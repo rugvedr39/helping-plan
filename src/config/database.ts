326376403fb1ba43d9046f0387cdaffa2df1e656
Rugved-Technify <rugvedr39@gmail.com>
2024-08-01 01:19:25 +0530

    port change

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,9 +1,6 @@
 import { Sequelize } from "sequelize";
 import * as fs from "fs";
 import * as path from "path";
-import { TransferHistory } from "../models/transferHistory.model";
-import { EPin } from "../models/epin.model";
-import { User } from "../models/User";
 
 const sequelize = new Sequelize({
   database: "defaultdb",
